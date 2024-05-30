cbf50a7d439e40ab243e2055c4237b642f43a245
leedonghyun <skydh507@ajou.ac.kr>
2024-05-30 18:15:15 +0900

    sovled p43163

--- src/programmers/dataStructure/p42861.java
+++ src/programmers/dataStructure/p42861.java
@@ -1,37 +1,42 @@
 package programmers.dataStructure;
-import java.util.*;
+
+import java.util.PriorityQueue;
+
 public class p42861 {
     class Solution {
         int[] parent;
 
-        private static class Edge implements Comparable<Edge>{
+        private static class Edge implements Comparable<Edge> {
             int start;
             int end;
             int cost;
-            public Edge(int start, int end, int cost){
+
+            public Edge(int start, int end, int cost) {
                 this.start = start;
                 this.end = end;
                 this.cost = cost;
             }
+
             @Override
-            public int compareTo(Edge e){
+            public int compareTo(Edge e) {
                 return this.cost - e.cost;
             }
         }
+
         public int solution(int n, int[][] costs) {
-            parent = new int[n+1];
-            for(int i=0; i<=n; i++){
+            parent = new int[n + 1];
+            for (int i = 0; i <= n; i++) {
                 parent[i] = i;
             }
             PriorityQueue<Edge> queue = new PriorityQueue<>();
-            for(int i=0; i<costs.length;i++){
-                queue.add(new Edge(costs[i][0],costs[i][1],costs[i][2]));
+            for (int i = 0; i < costs.length; i++) {
+                queue.add(new Edge(costs[i][0], costs[i][1], costs[i][2]));
             }
             int totalEdge = 0;
             int totalCost = 0;
-            while(totalEdge < n - 1){
+            while (totalEdge < n - 1) {
                 Edge edge = queue.poll();
-                if(find(edge.start) != find(edge.end)){
+                if (find(edge.start) != find(edge.end)) {
                     totalEdge++;
                     totalCost += edge.cost;
                     union(edge.start, edge.end);
@@ -39,16 +44,17 @@ public class p42861 {
             }
             return totalCost;
         }
-        private void union(int a ,int b){
+
+        private void union(int a, int b) {
             int x = find(a);
             int y = find(b);
-            if(x != y){
+            if (x != y) {
                 parent[y] = x;
             }
         }
 
-        private int find(int k){
-            if(parent[k]==k){
+        private int find(int k) {
+            if (parent[k] == k) {
                 return k;
             }
             return find(parent[k]);
