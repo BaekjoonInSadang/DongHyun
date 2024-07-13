2b6c6dd5721f16bbdcd4a7c6829e65e13f5a9181
leedonghyun <skydh507@ajou.ac.kr>
2024-07-13 17:11:03 +0900

    solved P12930

--- src/programmers/string/P12930.java
+++ src/programmers/string/P12930.java
@@ -1,2 +1,27 @@
-package programmers.string;public class P12930 {
+package programmers.string;
+
+public class P12930 {
+
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        String[] strs = s.split("");
+        int idx = 0;
+        for (int i = 0; i < strs.length; i++) {
+            if (strs[i].equals(" ")) {
+                idx = 1;
+                sb.append(strs[i]);
+                idx++;
+                continue;
+            }
+            if (idx % 2 == 0) {
+                sb.append(strs[i].toUpperCase());
+            } else {
+                sb.append(strs[i].toLowerCase());
+            }
+            idx++;
+        }
+
+        return sb.toString();
+    }
 }
+
