a82f4bfa92b9940569fe486d56f250991558ed94
leedonghyun <skydh507@ajou.ac.kr>
2024-07-14 16:10:05 +0900

    solved P84512

---
+++ src/programmers/recursion/P84512.java
@@ -0,0 +1,28 @@
+package programmers.recursion;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P84512 {
+    int index = 1;
+    Map<String, Integer> dict = new HashMap<>();
+    public static final String[] ALPHABET = {"A", "E", "I", "O", "U"};
+
+    public int solution(String word) {
+        setDict("");
+        int answer = dict.get(word);
+        return answer;
+    }
+
+    private void setDict(String str) {
+        if (str.length() == 5) {
+            return;
+        }
+        for (int i = 0; i < 5; i++) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(str).append(ALPHABET[i]);
+            dict.put(sb.toString(), index++);
+            setDict(sb.toString());
+        }
+    }
+}
