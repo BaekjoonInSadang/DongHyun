eb943b4d4bb953b757df51b21dbc482efb275003
leedonghyun <skydh507@ajou.ac.kr>
2024-07-14 14:55:41 +0900

    solved P12918

---
+++ src/programmers/string/P12918.java
@@ -0,0 +1,19 @@
+package programmers.string;
+
+public class P12918 {
+
+    public boolean solution(String s) {
+        if (s.length() != 4 && s.length() != 6) {
+            return false;
+        }
+        boolean answer = true;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!Character.isDigit(ch)) {
+                answer = false;
+                break;
+            }
+        }
+        return answer;
+    }
+}
